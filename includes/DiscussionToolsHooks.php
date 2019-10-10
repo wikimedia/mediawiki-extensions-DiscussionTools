53fae977233230d59710f81f6e983de3a0b3cc25
Ed Sanders <esanders@wikimedia.org>
2019-10-10 20:11:07 +0100

    Add an empty hook to load discussion tools on talk pages
    Change-Id: Iafa24ed41c06d4bd8b9a7a684b986fdb37aa87c7

--- includes/DiscussionToolsHooks.php
+++ includes/DiscussionToolsHooks.php
@@ -6,4 +6,25 @@
  * @ingroup Extensions
  * @license MIT
  */
-class DiscussionToolsHooks {}
+class DiscussionToolsHooks {
+	/**
+	 * Adds DiscussionTools JS to the output.
+	 *
+	 * This is attached to the MediaWiki 'BeforePageDisplay' hook.
+	 *
+	 * @param OutputPage $output The page view.
+	 * @param Skin $skin The skin that's going to build the UI.
+	 */
+	public static function onBeforePageDisplay( OutputPage $output, Skin $skin ) {
+		$title = $output->getTitle();
+		if (
+			// Only wikitext pages (e.g. not Flow boards)
+			$title->getContentModel() === CONTENT_MODEL_WIKITEXT &&
+			$title->isTalkPage()
+			// TODO: Allow non talk pages to be treated as talk pages
+			// using a magic word.
+		) {
+			// TODO: Load talk page enhancements
+		}
+	}
+}
