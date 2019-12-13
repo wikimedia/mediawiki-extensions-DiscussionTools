wikimedia/mediawiki-extensions-DiscussionTools

commit 49edbb82ab8b1a9358f6835d755bdccf5be0b398

Prompt users when pressing 'cancel' with content in the widget
And actually discard the contents when they confirm.

For now this uses the generic editor message, but that can
be tweaked later.

Bug: T240271
Change-Id: I2dfa19b2cc7ac49d7efea37ac8c9429c75934a91

modules/dt.ui.ReplyWidget.js
@@ -45,7 +45,7 @@ function ReplyWidget( comment, config ) {
 
 	// Events
 	this.replyButton.connect( this, { click: 'onReplyClick' } );
-	this.cancelButton.connect( this, { click: 'teardown' } );
+	this.cancelButton.connect( this, { click: [ 'teardown', true ] } );
 	this.$element.on( 'keydown', this.onKeyDown.bind( this ) );
 	this.beforeUnloadHandler = this.onBeforeUnload.bind( this );
 
@@ -101,10 +101,25 @@ ReplyWidget.prototype.setup = function () {
 	this.bindBeforeUnloadHandler();
 };
 
-ReplyWidget.prototype.teardown = function () {
-	// TODO: OOUI prompt if !empty
-	this.unbindBeforeUnloadHandler();
-	this.emit( 'teardown' );
+ReplyWidget.prototype.teardown = function ( confirm ) {
+	var promise,
+		widget = this;
+	if ( confirm && !this.isEmpty() ) {
+		// TODO: Override messages in dialog to be more ReplyWidget specific
+		promise = OO.ui.getWindowManager().openWindow( 'abandonedit' )
+			.closed.then( function ( data ) {
+				if ( !( data && data.action === 'discard' ) ) {
+					return $.Deferred().reject().promise();
+				}
+			} );
+	} else {
+		promise = $.Deferred().resolve().promise();
+	}
+	promise.then( function () {
+		widget.unbindBeforeUnloadHandler();
+		widget.clear();
+		widget.emit( 'teardown' );
+	} );
 };
 
 ReplyWidget.prototype.focus = function () {
@@ -238,5 +253,9 @@ ReplyWidget.prototype.onReplyClick = function () {
 		widget.cancelButton.setDisabled( false );
 	} );
 };
+
+/* Window registration */
+
+OO.ui.getWindowManager().addWindows( [ new mw.widgets.AbandonEditDialog() ] );
 
 module.exports = ReplyWidget;

commit 5eea2d42fb36e11604a0684bb3bad295716956bd

ReplyWidget: calling undefined clear method
Caused by 49edbb82ab8b1a9358f6835d755bdccf5be0b398.

Bug: T240729
Change-Id: I23bf7fcb7e0265f9770629590702a07a3dbed2b1

modules/dt.ui.ReplyWidget.js
@@ -126,6 +126,10 @@ ReplyWidget.prototype.focus = function () {
 	this.textWidget.focus();
 };
 
+ReplyWidget.prototype.clear = function () {
+	this.textWidget.setValue( '' );
+};
+
 ReplyWidget.prototype.onKeyDown = function ( e ) {
 	if ( e.which === OO.ui.Keys.ESCAPE ) {
 		this.emit( 'cancel' );