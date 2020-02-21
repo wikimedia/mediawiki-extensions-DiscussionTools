e9c401e3aa289ba0449b4744a3ae364e665c651f
Bartosz Dziewoński <matma.rex@gmail.com>
2020-02-21 21:11:19 +0100

    Ignore LRM and RLM before timezone indicator
    They are not generated by MediaWiki, but they often appear when users
    sign others' unsigned comments by copy-pasting the timestamp from the
    history page.
    
    Add test config data for nlwiki, exported by running this in the
    browser console:
    
      copy(
        JSON.stringify( { wgArticlePath, wgNamespaceIds, wgFormattedNamespaces }, null, 2 ) + '\n' +
        JSON.stringify( mw.loader.moduleRegistry['ext.discussionTools.parser'].packageExports['data.json'], null, 2 )
      );
    
    Bug: T245784
    Change-Id: Icbcdc5a028e9ce2cb09173f87769e525ec3082fc

--- tests/qunit/parser.test.js
+++ tests/qunit/parser.test.js
@@ -13,37 +13,37 @@ QUnit.test( '#getTimestampRegexp', function ( assert ) {
 	cases = [
 		{
 			format: 'H:i, j F Y',
-			expected: '(\\d{2}):(\\d{2}), (\\d{1,2}) (January|February|March|April|May|June|July|August|September|October|November|December) (\\d{4}) \\((UTC)\\)',
+			expected: '(\\d{2}):(\\d{2}), (\\d{1,2}) (January|February|March|April|May|June|July|August|September|October|November|December) (\\d{4})[\\u200E\\u200F]? [\\u200E\\u200F]?\\((UTC)\\)',
 			message: '(en) Boring'
 		},
 		{
 			format: 'H:i، j xg Y',
-			expected: '(\\d{2}):(\\d{2})، (\\d{1,2}) (January|February|March|April|May|June|July|August|September|October|November|December) (\\d{4}) \\((UTC)\\)',
+			expected: '(\\d{2}):(\\d{2})، (\\d{1,2}) (January|February|March|April|May|June|July|August|September|October|November|December) (\\d{4})[\\u200E\\u200F]? [\\u200E\\u200F]?\\((UTC)\\)',
 			message: '(ar) "xg" specifier'
 		},
 		{
 			format: 'H:i, j F xkY',
-			expected: '(\\d{2}):(\\d{2}), (\\d{1,2}) (January|February|March|April|May|June|July|August|September|October|November|December) (\\d{4}) \\((UTC)\\)',
+			expected: '(\\d{2}):(\\d{2}), (\\d{1,2}) (January|February|March|April|May|June|July|August|September|October|November|December) (\\d{4})[\\u200E\\u200F]? [\\u200E\\u200F]?\\((UTC)\\)',
 			message: '(th) "xkY" specifier'
 		},
 		{
 			format: 'H"h"i"min" "de" j "de" F "de" Y',
-			expected: '(\\d{2})h(\\d{2})min de (\\d{1,2}) de (January|February|March|April|May|June|July|August|September|October|November|December) de (\\d{4}) \\((UTC)\\)',
+			expected: '(\\d{2})h(\\d{2})min de (\\d{1,2}) de (January|February|March|April|May|June|July|August|September|October|November|December) de (\\d{4})[\\u200E\\u200F]? [\\u200E\\u200F]?\\((UTC)\\)',
 			message: '(pt) Escaped text (quotes)'
 		},
 		{
 			format: 'H\\hi\\m\\i\\n \\d\\e j \\d\\e F \\d\\e Y',
-			expected: '(\\d{2})h(\\d{2})min de (\\d{1,2}) de (January|February|March|April|May|June|July|August|September|October|November|December) de (\\d{4}) \\((UTC)\\)',
+			expected: '(\\d{2})h(\\d{2})min de (\\d{1,2}) de (January|February|March|April|May|June|July|August|September|October|November|December) de (\\d{4})[\\u200E\\u200F]? [\\u200E\\u200F]?\\((UTC)\\)',
 			message: '(pt) Escaped text (backslashes)'
 		},
 		{
 			format: 'j F Y à H:i',
-			expected: '(\\d{1,2}) (January|February|March|April|May|June|July|August|September|October|November|December) (\\d{4}) à (\\d{2}):(\\d{2}) \\((UTC)\\)',
+			expected: '(\\d{1,2}) (January|February|March|April|May|June|July|August|September|October|November|December) (\\d{4}) à (\\d{2}):(\\d{2})[\\u200E\\u200F]? [\\u200E\\u200F]?\\((UTC)\\)',
 			message: '(fr) Unescaped text (non-ASCII)'
 		},
 		{
 			format: 'Y年n月j日 (D) H:i',
-			expected: '(\\d{4})年(\\d{1,2})月(\\d{1,2})日 \\((Sun|Mon|Tue|Wed|Thu|Fri|Sat)\\) (\\d{2}):(\\d{2}) \\((UTC)\\)',
+			expected: '(\\d{4})年(\\d{1,2})月(\\d{1,2})日 \\((Sun|Mon|Tue|Wed|Thu|Fri|Sat)\\) (\\d{2}):(\\d{2})[\\u200E\\u200F]? [\\u200E\\u200F]?\\((UTC)\\)',
 			message: '(ja) Unescaped regexp special characters'
 		}
 	];
@@ -185,6 +185,13 @@ QUnit.test( 'Integration tests', function ( assert ) {
 			expected: require( './cases/no-heading/no-heading.json' ),
 			config: require( './data/enwiki-config.json' ),
 			data: require( './data/enwiki-data.json' )
+		},
+		{
+			name: 'Manually added signature with LRM',
+			dom: mw.template.get( 'test.DiscussionTools', 'cases/lrm-signature/lrm-signature.html' ).render(),
+			expected: require( './cases/lrm-signature/lrm-signature.json' ),
+			config: require( './data/nlwiki-config.json' ),
+			data: require( './data/nlwiki-data.json' )
 		}
 	];
 
