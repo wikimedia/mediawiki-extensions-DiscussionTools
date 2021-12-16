d0eec56f6d03cfa59fe410387f165e6a7aa620c6
Bartosz Dziewoński <matma.rex@gmail.com>
2021-12-16 12:51:04 +0100

    Show a hint on legacy interface when arriving from new topic tool hint
    Bug: T293962
    Change-Id: I56823efb4e58839b3f121ff23d576984b86e4e81

--- includes/Hooks/PageHooks.php
+++ includes/Hooks/PageHooks.php
@@ -18,16 +18,20 @@ use MediaWiki\Extension\DiscussionTools\CommentFormatter;
 use MediaWiki\Extension\DiscussionTools\SubscriptionStore;
 use MediaWiki\Hook\BeforePageDisplayHook;
 use MediaWiki\Hook\OutputPageBeforeHTMLHook;
+use MediaWiki\Hook\TitleGetEditNoticesHook;
 use MediaWiki\Page\Hook\ArticleViewHeaderHook;
 use MediaWiki\Page\Hook\BeforeDisplayNoArticleTextHook;
 use MediaWiki\User\UserNameUtils;
 use MediaWiki\User\UserOptionsLookup;
 use OOUI\ButtonWidget;
+use OOUI\HtmlSnippet;
+use OOUI\MessageWidget;
 use OutputPage;
 use ParserOutput;
 use RequestContext;
 use Skin;
 use SpecialPage;
+use Title;
 use VisualEditorHooks;
 
 class PageHooks implements
@@ -35,7 +39,8 @@ class PageHooks implements
 	BeforeDisplayNoArticleTextHook,
 	BeforePageDisplayHook,
 	GetActionNameHook,
-	OutputPageBeforeHTMLHook
+	OutputPageBeforeHTMLHook,
+	TitleGetEditNoticesHook
 {
 	/** @var ConfigFactory */
 	private $configFactory;
@@ -340,6 +345,50 @@ class PageHooks implements
 					'classes' => [ 'ext-discussiontools-init-new-topic' ]
 				] )
 			);
+		}
+	}
+
+	/**
+	 * @param Title $title Title object for the page the edit notices are for
+	 * @param int $oldid Revision ID that the edit notices are for (or 0 for latest)
+	 * @param array &$notices Array of notices. Keys are i18n message keys, values are
+	 *   parseAsBlock()ed messages.
+	 * @return bool|void True or no return value to continue or false to abort
+	 */
+	public function onTitleGetEditNotices( $title, $oldid, &$notices ) {
+		$context = RequestContext::getMain();
+
+		if (
+			// Hint is active
+			$this->userOptionsLookup->getOption( $context->getUser(), 'discussiontools-newtopictool-hint-shown' ) &&
+			// Turning off the new topic tool also dismisses the hint
+			$this->userOptionsLookup->getOption( $context->getUser(), 'discussiontools-' . HookUtils::NEWTOPICTOOL ) &&
+			// Only show when following the link from the new topic tool, never on normal edit attempts.
+			// This can be called from within ApiVisualEditor, so we can't access most request parameters
+			// for the main request. However, we can access 'editintro', because it's passed to the API.
+			$context->getRequest()->getVal( 'editintro' ) === 'mw-dt-topic-hint'
+		) {
+			$context->getOutput()->enableOOUI();
+
+			$returnUrl = $title->getFullURL( [
+				'action' => 'edit',
+				'section' => 'new',
+				'dtenable' => '1',
+			] );
+			$prefUrl = SpecialPage::getTitleFor( 'Preferences' )
+				->createFragmentTarget( 'mw-prefsection-editing-discussion' )->getFullURL();
+
+			$topicHint = new MessageWidget( [
+				'label' => new HtmlSnippet( wfMessage( 'discussiontools-newtopic-legacy-hint-return' )
+					->params( $returnUrl, $prefUrl )->parse() ),
+				'icon' => 'article',
+				'classes' => [ 'ext-discussiontools-ui-newTopic-hint-return' ],
+			] );
+
+			// Add our notice above the built-in ones
+			$notices = [
+				'discussiontools-newtopic-legacy-hint-return' => (string)$topicHint,
+			] + $notices;
 		}
 	}
 }
