2a31eed8465dc841829f0f79aa643cfac584b285
Ed Sanders <esanders@wikimedia.org>
2022-07-07 13:42:12 +0100

    CommentController: Scroll before focusing in showAndFocus
    This removes the need for a setTimeout before scrolling.
    
    Change-Id: I1c47d5635f66ed523b492c384464e2e6accabf01

--- modules/CommentController.js
+++ modules/CommentController.js
@@ -346,12 +346,10 @@ CommentController.prototype.focus = function () {
 
 CommentController.prototype.showAndFocus = function () {
 	var commentController = this;
-	this.focus();
-	// Calling can trigger a scroll-into-view within VE, so wait for this to
-	// happen so it doesn't cancel our scrolling of the whole widget into view
-	setTimeout( function () {
-		commentController.replyWidget.scrollElementIntoView( { padding: scrollPadding } );
-	} );
+	this.replyWidget.scrollElementIntoView( { padding: scrollPadding } )
+		.then( function () {
+			commentController.focus();
+		} );
 };
 
 CommentController.prototype.teardown = function ( mode ) {
