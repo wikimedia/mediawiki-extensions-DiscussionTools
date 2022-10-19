ed23ff1ad7bcbfc4e991f56557bf0015883fc349
Bartosz Dziewoński <matma.rex@gmail.com>
2022-10-19 16:11:02 +0200

    ThreadItemStoreTest: Use a consistent ordering for the output
    In preparation for changes affecting how the data is stored (T321121).
    
    Change-Id: I1d6edeb016431dd33e50cfbea1533a92b056a2a7

--- tests/phpunit/ThreadItemStoreTest.php
+++ tests/phpunit/ThreadItemStoreTest.php
@@ -57,19 +57,21 @@ class ThreadItemStoreTest extends IntegrationTestCase {
 		$expected = [];
 		$actual = [];
 		$tables = [
-			'discussiontools_items',
-			'discussiontools_item_pages',
-			'discussiontools_item_revisions',
-			'discussiontools_item_ids',
+			'discussiontools_items' => [ 'it_id' ],
+			'discussiontools_item_pages' => [ 'itp_id' ],
+			// We reuse rows causing the primary key to be all out of order.
+			// Use a consistent ordering for the output here.
+			'discussiontools_item_revisions' => [ 'itr_revision_id', 'itr_items_id', 'itr_itemid_id' ],
+			'discussiontools_item_ids' => [ 'itid_id' ],
 		];
-		foreach ( $tables as $table ) {
+		foreach ( $tables as $table => $order ) {
 			$expected[$table] = static::getJson( "../$dir/$table.json", true );
 
 			$res = wfGetDb( DB_REPLICA )->newSelectQueryBuilder()
 				->from( $table )
 				->field( '*' )
 				->caller( __METHOD__ )
-				->orderBy( 1 )
+				->orderBy( $order )
 				->fetchResultSet();
 			foreach ( $res as $i => $row ) {
 				foreach ( $row as $key => $val ) {
@@ -80,7 +82,7 @@ class ThreadItemStoreTest extends IntegrationTestCase {
 
 		// Optionally write updated content to the JSON files
 		if ( getenv( 'DISCUSSIONTOOLS_OVERWRITE_TESTS' ) ) {
-			foreach ( $tables as $table ) {
+			foreach ( $tables as $table => $order ) {
 				static::overwriteJsonFile( "../$dir/$table.json", $actual[$table] );
 			}
 		}
