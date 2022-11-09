08e1073f58b690b039c16f28679a6a5367e6cd4c
Bartosz Dziewoński <matma.rex@gmail.com>
2022-11-09 01:12:13 +0100

    ThreadItemStore: Handle race conditions when finding/inserting outside of transaction
    Bug: T322701
    Change-Id: Icea63dd562c1812a372f3a947ba1244e72d07a16

--- includes/ThreadItemStore.php
+++ includes/ThreadItemStore.php
@@ -369,11 +369,18 @@ class ThreadItemStore {
 				->caller( $method )
 				->fetchField();
 			if ( $itemIdsId === false ) {
-				$dbw->insert(
+				// Use upsert() instead of insert() to handle race conditions (T322701).
+				// Do a SELECT first to avoid unnecessary replication and bumping auto-increment values.
+				// (We can't just use INSERT IGNORE and then do a SELECT because of implicit
+				// transactions with REPEATABLE READ.)
+				$dbw->upsert(
 					'discussiontools_item_ids',
 					[
 						'itid_itemid' => $item->getId(),
 					],
+					'itid_itemid',
+					// We update nothing, as the rows will be identical, but this can't be empty
+					[ 'itid_id = itid_id' ],
 					$method
 				);
 				$itemIdsId = $dbw->insertId();
@@ -393,7 +400,11 @@ class ThreadItemStore {
 				->caller( $method )
 				->fetchField();
 			if ( $itemsId === false ) {
-				$dbw->insert(
+				// Use upsert() instead of insert() to handle race conditions (T322701).
+				// Do a SELECT first to avoid unnecessary replication and bumping auto-increment values.
+				// (We can't just use INSERT IGNORE and then do a SELECT because of implicit
+				// transactions with REPEATABLE READ.)
+				$dbw->upsert(
 					'discussiontools_items',
 					[
 						'it_itemname' => $item->getName(),
@@ -404,6 +415,9 @@ class ThreadItemStore {
 						'it_actor' =>
 							$this->actorStore->findActorIdByName( $item->getAuthor(), $dbw ),
 					] : [] ),
+					'it_itemname',
+					// We update nothing, as the rows will be identical, but this can't be empty
+					[ 'it_id = it_id' ],
 					$method
 				);
 				$itemsId = $dbw->insertId();
