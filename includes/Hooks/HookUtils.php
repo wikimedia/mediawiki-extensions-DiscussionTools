47118f901a3b2739b74e7e079d5781dfdebfc4a3
daniel <dkinzler@wikimedia.org>
2023-01-13 14:33:02 +0100

    Reduce parsoid PC writes to randomized sample
    We need to be careful about flooding the parser cache with parsoid
    content. For this reason, we currently only write to PC for a certain
    sample of edits. This logic is implemented in core in
    ParsoidHandler::allowParserCacheWrite and controlled by the
    TemporaryParsoidHandlerParserCacheWriteRatio setting.
    
    DiscussionTools triggers parsoid PC writes when handling the
    RevisionDataUpdates hook, so it should use the same sampling.
    
    Change-Id: Ic33f57b10ae53f431a3c3484c4853e88bf80f47a

--- includes/Hooks/HookUtils.php
+++ includes/Hooks/HookUtils.php
@@ -115,6 +115,15 @@ class HookUtils {
 
 		$parserOptions = ParserOptions::newFromAnon();
 
+		// HACK: remove before the release of MW 1.40 / early 2023.
+		if ( $mainConfig->has( 'TemporaryParsoidHandlerParserCacheWriteRatio' ) ) {
+			// We need to be careful about ramping up the cache writes,
+			// so we don't run out of disk space.
+			if ( wfRandom() >= $mainConfig->get( 'TemporaryParsoidHandlerParserCacheWriteRatio' ) ) {
+				$updateParserCacheFor = false;
+			}
+		}
+
 		if ( $updateParserCacheFor ) {
 			// $updateParserCache contains the name of the calling method
 			$parserOptions->setRenderReason( $updateParserCacheFor );
