wikimedia/mediawiki-extensions-DiscussionTools

commit 699868cd5b1b97b9fd8e4e45ff68b1c9a9f0a13f

Mark topic subscriptions as conflicting with CD gadget
The CD gadget provides subscription buttons (both topic
and page level), so with DT enabled these are duplicated.

Change-Id: I039878785e9df9f1b6384952b896bfa37710a151

includes/Hooks/HookUtils.php
@@ -72,6 +72,7 @@ class HookUtils {
 
 	public const FEATURES_CONFLICT_WITH_GADGET = [
 		self::REPLYTOOL,
+		self::TOPICSUBSCRIPTION,
 	];
 
 	private const CACHED_PAGE_PROPS = [

commit 808fc3d69e2504a95e1eef5874813e22f9e5b0b3

PreferenceHooks: Standardize dependency checking of features
The bug was introduced with commit 699868c, where the
topicsubscription feature was added as conflicting with Gadget.

The long special casing code is hard to read and maintain, let's
standardize it.

Bug: T388313
Change-Id: I2ad8d7f9f363277932ad2458b53e93aa3ecef476

includes/Hooks/HookUtils.php
@@ -75,6 +75,16 @@ class HookUtils {
 		self::TOPICSUBSCRIPTION,
 	];
 
+	public const FEATURES_DEPENDENCIES = [
+		self::SOURCEMODETOOLBAR => [
+			self::REPLYTOOL,
+			self::NEWTOPICTOOL,
+		],
+		self::AUTOTOPICSUB => [
+			self::TOPICSUBSCRIPTION,
+		]
+	];
+
 	private const CACHED_PAGE_PROPS = [
 		'newsectionlink',
 		'nonewsectionlink',

includes/Hooks/PreferenceHooks.php
@@ -122,36 +122,27 @@ class PreferenceHooks implements
 			}
 		}
 
-		if ( isset( $preferences['discussiontools-' . HookUtils::SOURCEMODETOOLBAR] ) && (
-			isset( $preferences['discussiontools-' . HookUtils::REPLYTOOL] ) ||
-			isset( $preferences['discussiontools-' . HookUtils::NEWTOPICTOOL] )
-		) ) {
-			// Disable this option when it would have no effect
-			// (both reply tool and new topic tool are disabled)
-			$preferences['discussiontools-' . HookUtils::SOURCEMODETOOLBAR]['disable-if'] = [ 'AND' ];
-
-			if ( isset( $preferences['discussiontools-' . HookUtils::REPLYTOOL] ) &&
-				// GlobalPreferences extension would delete disabled fields, avoid referring to it.
-				!( $preferences['discussiontools-' . HookUtils::REPLYTOOL]['disabled'] ?? false )
-			) {
-				$preferences['discussiontools-' . HookUtils::SOURCEMODETOOLBAR]['disable-if'][] = [
-					'===', 'discussiontools-' . HookUtils::REPLYTOOL, ''
-				];
+		foreach ( HookUtils::FEATURES_DEPENDENCIES as $feature => $dependencies ) {
+			if ( !isset( $preferences["discussiontools-$feature"] ) ) {
+				continue;
 			}
-			if ( isset( $preferences['discussiontools-' . HookUtils::NEWTOPICTOOL] ) ) {
-				$preferences['discussiontools-' . HookUtils::SOURCEMODETOOLBAR]['disable-if'][] = [
-					'===', 'discussiontools-' . HookUtils::NEWTOPICTOOL, ''
-				];
+			$fields = array_filter( $dependencies,
+				static fn ( $dep ) => isset( $preferences["discussiontools-$dep"] ) &&
+					// GlobalPreferences would remove disabled fields, avoid referencing them
+					!( $preferences["discussiontools-$dep"]['disabled'] ?? false )
+			);
+			// Disable the option when it would have no effect: its dependencies
+			// are all disabled by the user or by the conflicting Gadget
+			if ( count( $fields ) ) {
+				$preferences["discussiontools-$feature"]['disable-if'] = [ 'AND' ];
+				foreach ( $fields as $field ) {
+					$preferences["discussiontools-$feature"]['disable-if'][] = [
+						'===', "discussiontools-$field", ''
+					];
+				}
+			} else {
+				$preferences["discussiontools-$feature"]['disabled'] = true;
 			}
-		}
-
-		if ( isset( $preferences['discussiontools-' . HookUtils::AUTOTOPICSUB] ) &&
-			isset( $preferences['discussiontools-' . HookUtils::TOPICSUBSCRIPTION] )
-		) {
-			// Disable automatic subscriptions when subscriptions are disabled
-			$preferences['discussiontools-' . HookUtils::AUTOTOPICSUB]['disable-if'] = [
-				'===', 'discussiontools-' . HookUtils::TOPICSUBSCRIPTION, ''
-			];
 		}
 
 		$preferences['discussiontools-showadvanced'] = [