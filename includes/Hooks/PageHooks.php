wikimedia/mediawiki-extensions-DiscussionTools

commit b93d0d64b0647bc6055856bfe9d65283e48231f3

Remove placeholders when features disabled
We should always be removing unused placeholders, but
now that they are custom elements, it is more important
as they can affect CSS selectors.

Bug: T397011
Change-Id: I7c337149aceeb237a836439456dc8e9b826b3799

includes/Hooks/PageHooks.php
@@ -269,6 +269,8 @@ class PageHooks implements
 			$text = CommentFormatter::postprocessTopicSubscription(
 				$text, $batchModifyElements, $output, $this->subscriptionStore, $isMobile, $visualEnhancementsEnabled
 			);
+		} else {
+			$text = CommentFormatter::removeTopicSubscription( $text, $batchModifyElements );
 		}
 
 		if ( HookUtils::isFeatureEnabledForOutput( $output, HookUtils::REPLYTOOL ) ) {
@@ -276,6 +278,8 @@ class PageHooks implements
 			$text = CommentFormatter::postprocessReplyTool(
 				$text, $batchModifyElements, $output, $isMobile, $visualEnhancementsReplyEnabled
 			);
+		} else {
+			$text = CommentFormatter::removeReplyTool( $text, $batchModifyElements );
 		}
 
 		if ( $visualEnhancementsEnabled ) {
@@ -305,6 +309,8 @@ class PageHooks implements
 				] );
 			}
 			$text = CommentFormatter::postprocessVisualEnhancements( $text, $batchModifyElements, $output, $isMobile );
+		} else {
+			$text = CommentFormatter::removeVisualEnhancements( $text, $batchModifyElements );
 		}
 
 		$text = $batchModifyElements->apply( $text );

includes/CommentFormatter.php
@@ -651,6 +651,27 @@ class CommentFormatter {
 	}
 
 	/**
+	 * Remove placeholders for topic subscription buttons (e.g. if the feature is disabled)
+	 */
+	public static function removeTopicSubscription(
+		string $text, BatchModifyElements &$batchModifyElements
+	): string {
+		$batchModifyElements->add(
+			static fn ( SerializerNode $node ): bool => $node->name === 'mw:dt-subscribebutton' ||
+				$node->name === 'dt-subscribebutton',
+			static fn () => ''
+		);
+		// Legacy HTML - can be removed after caches have cleared
+		$text = preg_replace(
+			'/<!--__(DTSUBSCRIBEBUTTON(?:DESKTOP|MOBILE))__(.*?)-->/',
+			'',
+			$text
+		);
+
+		return $text;
+	}
+
+	/**
 	 * Replace placeholders for reply links with the real thing.
 	 */
 	public static function postprocessReplyTool(
@@ -733,6 +754,28 @@ class CommentFormatter {
 				$node->attrs['title'] = $relativeTime;
 				return $node;
 			}
+		);
+
+		return $text;
+	}
+
+	/**
+	 * Remove placeholders for reply links (e.g. if the feature is disabled)
+	 */
+	public static function removeReplyTool(
+		string $text, BatchModifyElements &$batchModifyElements,
+	): string {
+		$batchModifyElements->add(
+			static fn ( SerializerNode $node ): bool => $node->name === 'mw:dt-replybuttonscontent' ||
+				$node->name === 'dt-replybuttonscontent',
+			static fn () => ''
+		);
+
+		// Legacy HTML - can be removed after caches have cleared
+		$text = preg_replace(
+			'/<!--__DTREPLYBUTTONSCONTENT__-->/',
+			'',
+			$text
 		);
 
 		return $text;
@@ -998,7 +1041,38 @@ class CommentFormatter {
 	}
 
 	/**
+	 * Remove visual enhancements features (e.g. if the feature is disabled)
+	 */
+	public static function removeVisualEnhancements(
+		string $text, BatchModifyElements &$batchModifyElements
+	): string {
+		$batchModifyElements->add(
+			static fn ( SerializerNode $node ): bool => in_array( $node->name, [
+				'mw:dt-latestcommentthread',
+				'mw:dt-commentcount',
+				'mw:dt-authorcount',
+				'mw:dt-ellipsisbutton',
+				// HACK: MobileFrontend's HtmlFormatter strips the mw: namespace
+				'dt-latestcommentthread',
+				'dt-commentcount',
+				'dt-authorcount',
+				'dt-ellipsisbutton',
+			] ),
+			static fn () => ''
+		);
+		// Legacy HTML - can be removed after caches have cleared
+		$text = preg_replace(
+			'/<!--__(DTLATESTCOMMENTTHREAD|DTCOMMENTCOUNT|DTAUTHORCOUNT|DTELLIPSISBUTTON)__(.*?)(__)?-->/',
+			'',
+			$text
+		);
+		return $text;
+	}
+
+	/**
 	 * Post-process visual enhancements features for page subtitle
+	 *
+	 * @return string|null HTML for page subtitle, null if nothing to show
 	 */
 	public static function postprocessVisualEnhancementsSubtitle(
 		ParserOutput $pout, IContextSource $contextSource

commit b6324d643d7a53da77bee33fd0dff4afa93b34e3

Only parse and process the HTML if it seems to contain our tags
Follow-up to b93d0d64b0647bc6055856bfe9d65283e48231f3, which caused
this work to be done on every page view, not just discussion pages.

Bug: T400115
Change-Id: Icb91253753f2d1ad3b7106ca7ffe13cde2b0aad5

includes/Hooks/PageHooks.php
@@ -313,7 +313,10 @@ class PageHooks implements
 			CommentFormatter::removeVisualEnhancements( $batchModifyElements );
 		}
 
-		$text = $batchModifyElements->apply( $text );
+		// Optimization: Only parse and process the HTML if it seems to contain our tags (T400115)
+		if ( str_contains( $text, '<mw:dt-' ) || str_contains( $text, '<dt-' ) ) {
+			$text = $batchModifyElements->apply( $text );
+		}
 
 		// Append empty state if the OutputPageParserOutput hook decided that we should.
 		// This depends on the order in which the hooks run. Hopefully it doesn't change.