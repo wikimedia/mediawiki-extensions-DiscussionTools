ff38fee6fb4e5aec26b08d47c3c58129388f9d29
Dreamy Jazz <wpgbrown@wikimedia.org>
2026-05-12 09:43:37 +0100

    Make DiscussionTools not show hCaptcha initially unless configured
    Why:
    * We want to stage the release of hCaptcha to the DiscussionTools
      interface and so need to not show the hCaptcha widget even if
      for a generic edit hCaptcha would be required
    ** This is because on WMF wikis we disable hCaptcha for
       unsupported interfaces like DiscussionTools
    
    What:
    * Add $wgDiscussionToolsHCaptchaRequiredForAllEdits which is
      false by default and when true will allow hCaptcha to be
      shown before the initial DT submit attempt (as long as
      ConfirmEdit is installed and hCaptcha is required for
      any edit to the page)
    ** Update dt.ui.ReplyWidget.js to make use of this config
       as specified above
    * Update the QUnit tests to check this
    
    Bug: T425955
    Change-Id: I19d0581b3cc24fa1bef18c162c46b036a14efb10
    (cherry picked from commit 690ea97680423bde76613ba31d1c93d27ca7623e)

--- modules/dt.ui.ReplyWidget.js
+++ modules/dt.ui.ReplyWidget.js
@@ -1206,7 +1206,7 @@ ReplyWidget.prototype.setInitialCaptcha = function () {
 
 	// Only hCaptcha currently supports being rendered without a save failure first
 	const neededCaptcha = mw.libs.confirmEdit.CaptchaWidget.static.captchaNeededForEdit();
-	if ( neededCaptcha !== 'hcaptcha' ) {
+	if ( neededCaptcha !== 'hcaptcha' || !dtConf.hCaptchaRequiredForAllEdits ) {
 		return;
 	}
 
